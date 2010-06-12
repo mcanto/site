cc8c285a481fa17c21ac02d361d4ad063e1324f7
mcanto <mjosecanto@gmail.com>
2010-06-11 20:51:21 -0500

    actualizacion de hoy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+	#attr_accessible :title, :body, :user_id
 	validates_presence_of :title, :body, :user_id	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :posts
 
   map.resources :users
+  
+  map.root	:controller => "home"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
